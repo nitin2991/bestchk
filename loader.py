f14d4c12e8fc0aac9c2b76b3d5bd9f3c66f5ab84
nitin2991 <nitin.kamboj510@gmail.com>
2025-01-26 14:07:36 +0530

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-BOT_TOKEN = '5836125737:AAEMvAxSeKA6O6ViOuay68v-u1suJ_IhfI'
+BOT_TOKEN = '8149870803:AAEGkM-GSTRCyZl5_-udrqK2CPbNv6odu-A'
 bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
